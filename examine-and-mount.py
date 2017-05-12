a3025034d5797c425b0803c65eb8f9b55686d153
Rick Henry <fredericmhenry@gmail.com>
2017-05-12 00:31:33 -0400

    Full mount functionality with some rescuing of problems.
    Works correctly unless the repository is locked.

--- examine-and-mount.py
+++ examine-and-mount.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # interactive file recovery for borg backup
+# NOTE: When not properly shutdown, this will leave a backup mounted and the repository locked and further backups will
+# not be possible until it is umounted. If placed on a system where backups are automated, a daemon or cron job should
+# check periodically for mounted borgfs.
 
 # TODO: import config file (borrow borgmatic's code).
-# TODO: automatically perform umount on exit
 
 # TODO: rescue failure to break lock
 # TODO: prune down available backups interactively.
 # TODO: functions for finding and extracting single file.
-import os, subprocess, pprint, re, sys, datetime
+import os, subprocess, pprint, re, sys, datetime, atexit
 repo = 'backup-test:borgmatic-test'
 borg_path = '/usr/bin/borg'
 borg_passphrase = 'testing123'
@@ -19,6 +21,7 @@ class Backup:
         self.name = name
         self.date_time = date_time
 
+    # human readable date for printing
     def pretty_date(self):
         if self.date_time.date() == datetime.date.today():
             return str(self.date_time.strftime("Today at %I:%M %p"))
@@ -27,9 +30,10 @@ class Backup:
         else:
             return str(self.date_time.strftime("%a, %b %d, %Y at %I:%M %p"))
 
+    # mount the backup.
     def mount(self, mount_point):
-        run = subprocess.run([my_path, 'mount', repo + '::' + self.name, mount_point], \
-                env=dict(os.environ, BORG_PASSPHRASE=my_passphrase))
+        run = subprocess.run([borg_path, 'mount', repo + '::' + self.name, mount_point], \
+                env=dict(os.environ, BORG_PASSPHRASE=borg_passphrase))
 
 
 def backup_list():
@@ -69,9 +73,26 @@ def choose_examine(backups):
     number = input("Type the number of the backup you would like to examine. ")
     backups[int(number)].mount(my_mountpoint)
 
+def cleanup():
+    subprocess.run([borg_path, 'umount', my_mountpoint])
 
+def done():
+    print("Your backup is available at", str(my_mountpoint) + '.', "Please copy any files and return to this program")
+    while True:
+        yn = input("Did you find what you were looking for?[y/n]")
+        try:
+            return bool(yn[0] == 'Y' or yn[0] == 'y')
+        except IndexError:
+            print("Please type [y]es or [n]o")
+
+
+atexit.register(cleanup)
 backups = store_backup_info(backup_list())
-# for b in backups: b.pretty_date
-choose_examine(backups)
-# for b in backups: print(b.name, b.datetime)
-
+while True:
+    choose_examine(backups)
+    if done():
+        print("Great. Your backup is being unmounted.")
+        break
+    else:
+        print("Let's try a different backup.")
+        cleanup()
