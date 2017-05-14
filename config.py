c9e3ee22c0233cbac9c7792b0460a46e42c0d2d0
Rick Henry <fredericmhenry@gmail.com>
2017-05-13 23:32:53 -0400

    Update config_example and add interactive creation
    config.py now includes interactive creation of the configuration file. Some
    section names have changed and an extraction point has been added for future
    implementation.
    config_example.conf has been updated to reflect these changes and anonymized.

--- config.py
+++ config.py
@@ -11,13 +11,42 @@ def parseconfig():
     if not os.path.isfile(config_path):
         print("No configuration file found. Let's create one now.")
 
+        config['Repo'] = {}
+        config['Recovery'] = {}
+        config['System'] = {}
+
+        config['Repo']['Path'] = input("Please enter the full path to your borg repository.\n")
+        config['Repo']['Passphrase'] = input("Please enter your borg repo passphrase.\n")
+
+        default_mnt_path = home + '/recover_mount'
+        yn = input("The default path for mounting backups is: {} . Is this ok?[yn]".format(default_mnt_path))
+        if yn[0] == 'n' or yn[0] == 'N':
+            config['Recovery']['Mountpoint'] = input("Please enter the mountpoint you would like to use.\n")
+        else:
+            config['Recovery']['Mountpoint'] = default_mnt_path
+
+        default_extract_path = home + '/recovered_files'
+        yn = input("The default path for mounting backups is: {} . Is this ok?[yn]".format(default_extract_path))
+        if yn[0] == 'n' or yn[0] == 'N':
+            config['Recovery']['Extractpoint'] = input("Please enter the folder you would like to use.\n")
+        else:
+            config['Recovery']['Extractpoint'] = default_extract_path
+
+        config['System']['OpenCommand'] = input("Please enter a shell command for opening files and folders.\n")
+        if not os.path.exists(home + '/.config/borg-examine'): os.makedirs(home + '/.config/borg-examine')
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+        print("Your configuration file has been written. It can be edited at any time at {}"\
+                .format(config_path))
+
     # read configuration file
     config.read(config_path)
 
     options = {'repopath': config['Repo']['Path'],
-                     'passphrase': config['Repo']['passphrase'],
-                     'mountpoint': config['Mount']['mountpoint']
-                    }
-    
+            'passphrase': config['Repo']['passphrase'],
+            'mountpoint': config['Recovery']['mountpoint'],
+            'opencommand': config['System']['opencommand']
+            }
+
     return options
 
