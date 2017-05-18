4c304d2b2ad2029e75379b7771b561a401ae4c93
Rick Henry <fredericmhenry@gmail.com>
2017-05-18 02:40:35 -0400

    Implement returning of found files
    Reorganized classes: created DatedInfo class with prettydate function to be
    inherited by Backup class and new FoundFile class. Files that match are now
    printed with their last modified time and an index.
    
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- get_backups.py
+++ get_backups.py
@@ -3,12 +3,13 @@
 
 import os, subprocess, datetime, re, sys
 
+######## CLASSES SECTION ##########
+
 # Exception raised when borg list is not successfully completed.
 class AccessError(Exception):
     pass
 
-# Class for storing information about backups in a repo.
-class Backup:
+class DatedInfo:
     def __init__(self, name, date_time):
         self.name = name
         self.date_time = date_time
@@ -22,6 +23,8 @@ class Backup:
         else:
             return str(self.date_time.strftime("%a, %b %d, %Y at %I:%M %p"))
 
+# Class for storing information about backups in a repo.
+class Backup(DatedInfo):
     # mount the backup.
     def mount(self, options):
         # A nice message in case it takes a while
@@ -40,25 +43,53 @@ class Backup:
                 env=dict(os.environ, BORG_PASSPHRASE=options['passphrase']))
         # store output and return code
         backup_list = make_list.communicate()[0].decode(sys.stdout.encoding),make_list.returncode
+
         # TODO: force it to sleep in case it is a timeout so that user doesn't
         # have to go through everything again. Or write progress to a file so
         # that it can be resumed from previous run.
+
         # catch errors
         if int(backup_list[1]) != 0: catch_borg_errors(backup_list)
-        files = file_regex.findall(str(backup_list[0]))
-        print("Here are the files that match your search in the chosen backup.")
-        for f in files:
-            print(f)
 
-def get_filename():
-    # get input and clean it for use in regex
+        # create FoundFile objects
+        raw_files = file_regex.findall(str(backup_list[0]))
+        all_files = parse_file_info(raw_files)
+        print("\nHere are the files that match your search in the chosen backup.")
+        i = 0
+        for f in all_files:
+            # number and format for printing
+            output = ['(' + str(i) + ')', f.name, 'LAST MODIFIED ' + f.pretty_date()]
+            print("{:<4} {:<90} {:>10}".format(*output))
+            i += 1
+
+
+
+class FoundFile(DatedInfo):
+    pass
+
+########## FILE FUNCTIONS SECTION ############ 
+def search_filename(): 
+    # get input and clean it for use in regex 
     filename = input("Please enter all or part of the filename you are looking for: ")
     filename = re.escape(filename.strip())
     # create regex that will return entire line from borg list based on user input
     file_regex = re.compile(r"^.*?" + filename + r".*?$", flags=re.IGNORECASE|re.MULTILINE)
     return file_regex
 
+def parse_file_info(file_array):
+    all_files = []
+    for f in file_array:
+        # fine date
+        raw_date = re.search(r'\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', f).group()
+        date_time = datetime.datetime.strptime(raw_date, ' %Y-%m-%d %H:%M:%S')
+        # find name after date
+        find_name = re.search(r':\d{2}\s.*', f).group()
+        # drop date part of regex
+        name = find_name[4:]
+        all_files.append(FoundFile(name, date_time))
+    return all_files
 
+########## BACKUP FUNCTIONS SECTIONS ############
 def backup_list(repopath, passphrase):
     # get list from backup repo
     run = subprocess.Popen(['borg', 'list', repopath], \
@@ -71,20 +102,6 @@ def backup_list(repopath, passphrase):
     arr_list = ret[0].splitlines()
     # return array, at this point just a text dump of each backup
     return arr_list
-
-def catch_borg_errors(ret):
-    # possible problems created running borg list. Hack-y, but it works.
-    messages = {
-            'LockTimeout': 'Cannot unlock repository. Backup may be in progress. Try again in a few minutes.',
-            'passphrase': 'Passphrase was rejected. Update config file and try again.',
-            'valid': 'The repository in your config file does not appear to be valid. Please correct it.',
-            'exist': 'The repository in your config file does not exist. Please correct.',
-            'remote': 'Connection to backup server failed. Check network connection.',
-            'other': 'An unknown error has occurred: ' + ret[0] 
-            }
-    # check for possible issues and raise an error if they arise.
-    for k, v in messages.items():
-        if k in ret[0]: raise AccessError(v)
 
 
 def parse_backup_info(backup_array):
@@ -102,3 +119,19 @@ def parse_backup_info(backup_array):
 
     # now they're nice objects
     return all_backups
+
+######## ERROR HANDLING FUNCTIONS #########
+def catch_borg_errors(ret):
+    # possible problems created running borg list. Hack-y, but it works.
+    messages = {
+            'LockTimeout': 'Cannot unlock repository. Backup may be in progress. Try again in a few minutes.',
+            'passphrase': 'Passphrase was rejected. Update config file and try again.',
+            'valid': 'The repository in your config file does not appear to be valid. Please correct it.',
+            'exist': 'The repository in your config file does not exist. Please correct.',
+            'remote': 'Connection to backup server failed. Check network connection.',
+            'other': 'An unknown error has occurred: ' + ret[0] 
+            }
+    # check for possible issues and raise an error if they arise.
+    for k, v in messages.items():
+        if k in ret[0]: raise AccessError(v)
+
