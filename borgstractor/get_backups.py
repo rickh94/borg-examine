806a9b27dc9754fbd12d07dfa55a740856c5b91f
Rick Henry <fredericmhenry@gmail.com>
2017-12-21 21:38:45 -0500

    intermediate commit
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- borgstractor/get_backups.py
+++ borgstractor/get_backups.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-# class and functions for retrieving backup information.
-
+"""Classes and functions for retrieving backup information and files."""
 import os
 import subprocess
 import datetime
@@ -11,11 +9,11 @@ import atexit
 from borgstractor import borg_command
 from borgstractor import settings
 
-######## CLASSES SECTION ##########
 
 # Exception raised when borg list is not successfully completed.
 class AccessError(Exception):
     pass
+
 
 class DatedInfo:
     def __init__(self, name, date_time):
@@ -27,13 +25,12 @@ class DatedInfo:
     def pretty_date(self):
         if self.date_time.date() == datetime.date.today():
             return str(self.date_time.strftime("Today at %I:%M %p"))
-        elif self.date_time.date() == (datetime.date.today() - datetime.timedelta(days=1)):
+        elif self.date_time.date() == (datetime.date.today()
+                                       - datetime.timedelta(days=1)):
             return str(self.date_time.strftime("Yesterday at %I:%M %p"))
         else:
             return str(self.date_time.strftime("%a, %b %d, %Y at %I:%M %p"))
-        # end if
-    # end def pretty_date
-# end class DatedInfo
+
 
 # Class for storing information about backups in a repo.
 class Backup(DatedInfo):
@@ -42,35 +39,37 @@ class Backup(DatedInfo):
         # A nice message in case it takes a while
         print("Please wait a moment, your backup is being retrieved.")
         # create the mountpoint if it doesn't already exist
-        if not os.path.exists(getattr(settings, 'mountpoint')): 
+        if not os.path.exists(getattr(settings, 'mountpoint')):
             os.mkdir(getattr(settings, 'mountpoint'))
         # mount the backup
-        run = borg_command.create('run', 'mount', self.name)
-    # end def mount
+        borg_command.create('run', 'mount', self.name)
 
     def extract_file(
-            self, 
+            self,
             # file_regex
             ):
         # TODO: move all of this into a function
         # get list of files from backup
         make_list = borg_command.create('Popen', 'list', self.name)
         # store output and return code
-        backup_list = make_list.communicate()[0].decode(sys.stdout.encoding),make_list.returncode
+        backup_list = (make_list.communicate()[0].decode(sys.stdout.encoding),
+                       make_list.returncode)
 
         # TODO: force it to sleep in case it is a timeout so that user doesn't
         # have to go through everything again. Or write progress to a file so
         # that it can be resumed from previous run.
 
         # catch errors
-        if int(backup_list[1]) != 0: catch_borg_errors(backup_list)
+        if int(backup_list[1]) != 0:
+            catch_borg_errors(backup_list)
 
-        # searches for initial list of files, creates objects, prints, returns array
+        # searches for initial list of files, creates objects, prints, returns
+        # array
         file_list = backup_list[0].split('\n')
         found_files = search_filename('enter search', file_list)
         # print(found_files)
         # raw_files = find_files(backup_list[0], file_regex)
-        all_files = parse_file_info(file_list)
+        all_files = parse_file_info(found_files)
         print(all_files)
         # NOTE: A lot of this flow control should be in the main function.
         # print_found_files(all_files)
@@ -176,28 +175,28 @@ class Backup(DatedInfo):
         #         # catastrophic problem. should never happen. kills program.
         #         print("Something has gone wrong.")
         #         sys.exit(1)
-                # end if / elif for non-number input
-            # end try (for user input)
-        # end while (user input)
-    # end def extract_file
 
     def extract(self, num, file_list):
         to_extract = file_list[num]
 
         # make extraction dir if not found and change to it
         ext_dir = getattr(settings, 'extractdir')
-        if not os.path.exists(ext_dir): os.mkdir(ext_dir)
+        if not os.path.exists(ext_dir):
+            os.mkdir(ext_dir)
 
         os.chdir('/tmp')
         # extract the file and rename it
         borg_command.create('run', 'extract', self.name, to_extract.name)
 
         full_extract = '/tmp/' + to_extract.name
-        extracted_file_name = os.path.basename(full_extract)
-        shutil.move(full_extract, ext_dir + '/' + to_extract.date_time.strftime("%Y-%m-%d_%H:%M:%S-") + extracted_file_name)
+        extracted_file_name = (
+            ext_dir + '/' +
+            to_extract.date_time.strftime("%Y-%m-%d_%H:%M:%S-") +
+            os.path.basename(full_extract))
+        shutil.move(full_extract, extracted_file_name)
 
         # wait for user confirmation to open restored file
-        trash = input("Press [enter] to see your extracted file.")
+        input("Press [enter] to see your extracted file.")
         subprocess.Popen([getattr(settings, 'opencommand'), ext_dir])
 
 
@@ -207,9 +206,9 @@ class Backup(DatedInfo):
 class FoundFile(DatedInfo):
     pass
 
-########## FILE FUNCTIONS SECTION ############ 
-def search_filename(message, list_of_files): 
-    # get input and clean it for use in regex 
+
+def search_filename(message, list_of_files):
+    # get input and clean it for use in regex
     while 1:
         filename = input(message)
         if filename != '':
@@ -235,6 +234,7 @@ def search_filename(message, list_of_files):
     # return file_regex
 # end def search_filename
 
+
 def parse_file_info(file_array):
     all_files = []
     for f in file_array:
@@ -243,7 +243,8 @@ def parse_file_info(file_array):
         if ' -> ' in f:
             continue
         # find date
-        raw_date = re.search(r'\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', f).group()
+        raw_date = re.search(
+            r'\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', f).group()
         date_time = datetime.datetime.strptime(raw_date, ' %Y-%m-%d %H:%M:%S')
         # find name after date
         find_name = re.search(r':\d{2}\s.*', f).group()
@@ -254,17 +255,16 @@ def parse_file_info(file_array):
     return all_files
 
 
-########## BACKUP FUNCTIONS SECTIONS ############
 def backup_list():
     # get list from backup repo
     run = borg_command.create('Popen', 'list')
-    ret = run.communicate()[0].decode(sys.stdout.encoding),run.returncode
+    ret = run.communicate()[0].decode(sys.stdout.encoding), run.returncode
     # stop program if list doesn't properly get a list
-    if int(ret[1]) != 0: catch_borg_errors(ret)
+    if int(ret[1]) != 0:
+        catch_borg_errors(ret)
     arr_list = ret[0].splitlines()
     # return array, at this point just a text dump of each backup
     return arr_list
-# end def backup_list
 
 
 def parse_backup_info(backup_array):
@@ -273,32 +273,31 @@ def parse_backup_info(backup_array):
     for backup in backup_array:
         # parse backup info
         name = re.match("[^\s]*", backup).group()
-        raw_date = re.search(r'\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', backup).group()
+        raw_date = re.search(
+            r'\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', backup).group()
         date_time = datetime.datetime.strptime(raw_date, ' %Y-%m-%d %H:%M:%S')
 
         # ignore checkpoints
         if re.match(".*\.checkpoint", name) is None:
             tmp = Backup(name, date_time)
             all_backups.append(tmp)
-        # add backup object to list
-    # end for loop (storing info)
 
     return all_backups
-# end def parse_backup_info
 
-# cleanup mounted filesystem
+
 def cleanup(mountpoint):
+    """Cleanup mounted filesystem."""
     subprocess.run(['borg', 'umount', mountpoint])
-# end cleanup
 
-# check if user found file and run again or exit.
+
 def done_mounting(mountpoint, opencommand):
-    print("Your backup is available at {}. It will now open for you to find the" \
-            .format(mountpoint), 
-            "files you need and copy them out of the backup. When you are done", 
-            "please return to this window to close the backup.")
+    """Check if user found file to determine next action."""
+    print("Your backup is available at {}. It will now open for you to find"
+          " the files you need and copy them out of the backup. When you are"
+          " done please return to this window to close the backup."
+          ).format(mountpoint)
     # wait for user
-    trash = input("Press [enter] to continue.")
+    input("Press [enter] to continue.")
     # open mounted directory
     subprocess.Popen([opencommand, mountpoint])
     while True:
@@ -308,19 +307,18 @@ def done_mounting(mountpoint, opencommand):
             return bool(yn[0] == 'Y' or yn[0] == 'y')
         except IndexError:
             print("Please type [y]es or [n]o")
-        # end try
-    # end while
-# end done
+
 
 def find_files(file_list, regex):
     while 1:
         raw_files = regex.findall(str(file_list))
         if len(raw_files) == 0:
             regex = search_filename("No matches found. Please enter a new" +
-                    " search: ")
+                                    " search: ")
         else:
             break
     return raw_files
+
 
 def print_found_files(file_list):
     print("Here are the files that match your search in the chosen backup.")
@@ -329,11 +327,11 @@ def print_found_files(file_list):
     while i < len(file_list):
         f = file_list[i]
         # offset i for printing and better UX
-        output = ['(' + str(i + 1) + ')', f.name, 'LAST MODIFIED ' + f.pretty_date()]
+        output = ['(' + str(i + 1) + ')',
+                  f.name, 'LAST MODIFIED ' + f.pretty_date()]
         print("{:<4} {:<90} {:>10}".format(*output))
         i += 1
-    # end while (printing files)
-# end def print_found_files
+
 
 def new_search(raw_file_list):
     search_regex = search_filename("\nPlease enter a new search term: ")
@@ -342,20 +340,24 @@ def new_search(raw_file_list):
     print_found_files(all_files)
     return new_files, all_files
 
-######## ERROR HANDLING FUNCTIONS #########
+
 def catch_borg_errors(ret):
     # possible problems created running borg list. Hack-y, but it works.
     messages = {
-            'LockTimeout': 'Cannot unlock repository. Backup may be in progress. Try again in a few minutes.',
-            'passphrase': 'Passphrase was rejected. Update config file and try again.',
-            'valid': 'The repository in your config file does not appear to be valid. Please correct it.',
-            'exist': 'The repository in your config file does not exist. Please correct.',
-            'remote': 'Connection to backup server failed. Check network connection.',
-            'other': 'An unknown error has occurred. Printing borg traceback:\n' + ret[0] 
-            }
+        'LockTimeout': ('Cannot unlock repository. Backup may be in progress.'
+                        ' Try again in a few minutes.'),
+        'passphrase': ('Passphrase was rejected. Update config file '
+                       ' and try again.'),
+        'valid': ('The repository in your config file does not appear '
+                  ' to be valid. Please correct it.'),
+        'exist': ('The repository in your config file does not exist.'
+                  ' Please correct.'),
+        'remote': ('Connection to backup server failed. '
+                   ' Check network connection.'),
+        'other': ('An unknown error has occurred. '
+                  ' Printing borg traceback:\n' + ret[0]),
+    }
     # check for possible issues and raise an error if they arise.
     for k, v in messages.items():
-        if k in ret[0]: raise AccessError(v)
-    # end for loop to raise error
-# end def catch_borg_errors
-
+        if k in ret[0]:
+            raise AccessError(v)
